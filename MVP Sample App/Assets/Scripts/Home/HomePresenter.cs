dd20c33f09bc0bebf53b4237763e19be89ec724b
YamamotoTomoyaSN <tomoya.yamamoto@scopenext.jp>
2023-06-19 23:18:12 +0900

    ポケモンAPIからポケモン画像を表示するアプリの作成

---
+++ MVP Sample App/Assets/Scripts/Home/HomePresenter.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Home {
+
+	public interface IHomeEventDelegate {
+		void GetPoketMonsterData(string idText);
+	}
+
+	public abstract class HomePresenterView : MonoBehaviour {
+		public abstract void Init();
+		public abstract void SetEventDelegate(IHomeEventDelegate eventDelegate);
+	}
+
+	public class HomePresenter : MonoBehaviour, IHomeEventDelegate {
+
+		[SerializeField] private HomeView m_View = null;
+
+		[SerializeField] private HomeModel m_Model = null;
+
+		void Start() {
+			m_View.Init();
+
+			m_View.SetEventDelegate(this);
+		}
+
+		public void GetPoketMonsterData(string idText) {
+			if (!int.TryParse(idText, out int id)) {
+				// 入力した内容がID(int)に変換できなかった場合
+				return;
+			}
+			Debug.Log("Search pokemon id:" + id);
+			// Modelにポケモン情報を取得依頼をする
+			StartCoroutine(m_Model.GetPoketMonsterDataAPI(id.ToString(), OnCompletedGetPoketMonsterData));
+		}
+
+		private void OnCompletedGetPoketMonsterData(Texture2D texture) {
+			// Viewにポケモンテクスチャを表示依頼
+			m_View.SetPokemonImage(texture);
+		}
+	}
+}
---
+++ MVP Sample App/Assets/Scripts/Home/HomeView.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Home {
+	public class HomeView : HomePresenterView {
+
+		[SerializeField]
+		private RawImage m_PokemonImange = null;
+
+		[SerializeField]
+		private TMP_InputField m_PokemonIdInputField = null;
+
+		[SerializeField]
+		private Button m_SearchButton = null;
+
+		private IHomeEventDelegate m_EventDelegate;
+
+		public override void Init() {
+			// Presenter側でViewの初期化処理が呼び出される
+			m_SearchButton.onClick.AddListener(OnSearchButton);
+		}
+
+		public override void SetEventDelegate(IHomeEventDelegate eventDelegate) {
+			// Presenter側でEventDelegateの登録処理が呼び出される
+
+			// Presenterに処理を依頼するためのインターフェースを変数に格納
+			m_EventDelegate = eventDelegate;
+		}
+
+		private void OnSearchButton() {
+			// 検索ボタン押下時
+			string id = m_PokemonIdInputField.text;
+			m_EventDelegate.GetPoketMonsterData(id);
+		}
+
+		public void SetPokemonImage(Texture2D texture) {
+			m_PokemonImange.texture = texture;
+		}
+	}
+}
